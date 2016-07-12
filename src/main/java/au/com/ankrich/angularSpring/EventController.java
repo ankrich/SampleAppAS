03ba4b25bf6ac324490dd5c93e9dbc84718624af
ankrich <ankit.richharia4@gmail.com>
2016-07-12 15:38:44 +1000

    added form based authentication - some open issues

--- src/main/java/au/com/ankrich/angularSpring/EventController.java
+++ src/main/java/au/com/ankrich/angularSpring/EventController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,5 +39,11 @@ public class EventController {
 
     }
 
+        @RequestMapping("/user")
+        public Principal user(Principal user) {
+            return user;
+        }
 
-}
+    }
+
+
--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,4 +1,4 @@
-angular.module('events', [])
+/*angular.module('events', [])
 .controller('showEvents', function($http){
 var self = this;
 $http.get('/events/').then(function(response){
@@ -6,5 +6,77 @@ $http.get('/events/').then(function(response){
 self.events = response.data;
 
 })
- })
+ })*/
+
+angular.module('events', [ 'ngRoute' ])
+  .config(function($routeProvider, $httpProvider) {
+
+    $routeProvider.when('/', {
+      templateUrl : 'home.html',
+      controller : 'showEvents',
+      controllerAs: 'controller'
+    }).when('/login', {
+      templateUrl : 'login.html',
+      controller : 'navigation',
+      controllerAs: 'controller'
+    }).otherwise('/');
+
+    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
+  })
+  .controller('showEvents', function($http) {
+    var self = this;
+    $http.get('/events/').then(function(response) {
+      self.events = response.data;
+    })
+  })
+  .controller('navigation', function($rootScope, $http, $location) {
+
+                              var self = this
+
+                              var authenticate = function(credentials, callback) {
+
+                                var headers = credentials ? {authorization : "Basic "
+                                    + btoa(credentials.username + ":" + credentials.password)
+                                } : {};
+
+                                $http.get('user', {headers : headers}).then(function(response) {
+                                  if (response.data.name) {
+                                    $rootScope.authenticated = true;
+                                  } else {
+                                    $rootScope.authenticated = false;
+                                  }
+                                  callback && callback();
+                                }, function() {
+                                  $rootScope.authenticated = false;
+                                  callback && callback();
+                                });
+
+                              }
+
+                              self.logout = function() {
+                                $http.post('logout', {}).finally(function() {
+                                  $rootScope.authenticated = false;
+                                  $location.path("/");
+                                });
+                              }
+
+
+                              authenticate();
+                                self.credentials = {};
+                                self.login = function() {
+                                    authenticate(self.credentials, function() {
+                                      if ($rootScope.authenticated) {
+                                        $location.path("/");
+                                        self.error = false;
+                                      } else {
+                                        $location.path("/login");
+                                        self.error = true;
+                                      }
+                                    });
+                                };
+                              });
+
+
+
 
